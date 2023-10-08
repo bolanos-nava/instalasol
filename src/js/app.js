dcb534f3b6ebb8be27387d774a179bbc9af4ab29
bolanos-nava <bolanos.nava@gmail.com>
2023-10-08 00:43:54 -0600

    Adds sidebar and JS behavior to toggle the sidebar

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,17 @@
 function toggleSidebar() {
     const sidebar = document.getElementById("sidebar");
-    const toggleSidebarButton = document.getElementById("hamburger-menu-button");
+    const buttonSidebarToggler = document.getElementById("buttonSidebarToggler");
+    const sidebarTogglerArrow = document.getElementById("sidebarTogglerArrow");
 
-    toggleSidebarButton.addEventListener("click", () => {
+    buttonSidebarToggler.addEventListener("click", () => {
         sidebar.classList.toggle("sidebar-close");
+        // for (const arrow of sidebarTogglerArrows) {
+        //     arrow.classList.toggle("visually-hidden");
+        // }
+        sidebarTogglerArrow.classList.toggle("rotateTogglerArrow");
+        // sidebarTogglerArrow.classList.replace("fa-arrow-left", "fa-arrow-right");
+        // console.log({ classList: sidebarTogglerArrow.classList });
+
     })
 }
 
