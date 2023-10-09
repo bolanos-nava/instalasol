cf789881890b0037c0a862abc6171132486c52cd
bolanos-nava <bolanos.nava@gmail.com>
2023-10-08 23:59:45 -0600

    Adds backdrop to sidebar

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,10 @@ function toggleSidebar() {
     const sidebar = document.getElementById("sidebar");
     const buttonSidebarToggler = document.getElementById("buttonSidebarToggler");
     const sidebarTogglerArrow = document.getElementById("sidebarTogglerArrow");
+    const sidebarBackdrop = document.getElementById("sidebarBackdrop");
 
     buttonSidebarToggler.addEventListener("click", () => {
+        sidebarBackdrop.classList.toggle("backdrop");
         sidebar.classList.toggle("sidebar-close");
         sidebarTogglerArrow.classList.toggle("rotateTogglerArrow");
     })
