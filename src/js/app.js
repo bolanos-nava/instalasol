1517de9e39cb647bcc563100a0debdc8ca2df626
bolanos-nava <bolanos.nava@gmail.com>
2023-10-11 19:48:54 -0600

    Corrects backdrop script and adds backdrop to header navbar

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,27 @@
 function toggleSidebar() {
     const sidebar = document.getElementById("sidebar");
-    const buttonSidebarToggler = document.getElementById("buttonSidebarToggler");
+    const buttonsNavbarToggler = document.getElementsByClassName("navibar__toggler");
     const sidebarTogglerArrow = document.getElementById("sidebarTogglerArrow");
-    const sidebarBackdrop = document.getElementById("sidebarBackdrop");
+    const navbarBackdrop = document.getElementById("navbarBackdrop");
 
-    buttonSidebarToggler.addEventListener("click", () => {
-        sidebarBackdrop.classList.toggle("backdrop");
+    const toggleBackdrop = () => navbarBackdrop.classList.toggle("backdrop-show");
+    const toggleSidebar = () => {
+        toggleBackdrop();
         sidebar.classList.toggle("sidebar-close");
-        sidebarTogglerArrow.classList.toggle("rotateTogglerArrow");
-    })
+        sidebarTogglerArrow.classList.toggle("rotate-toggler-arrow");
+    }
+    const toggleHeaderNavbar = toggleBackdrop;
+
+    for (const buttonToggler of buttonsNavbarToggler) {
+        switch (buttonToggler.id) {
+            case "buttonSidebarToggler":
+                buttonToggler.addEventListener("click", toggleSidebar);
+                break;
+            case "buttonHeaderNavbarToggler":
+                buttonToggler.addEventListener("click", toggleSidebar);
+                break;
+        }
+    }
 }
 
 toggleSidebar();
